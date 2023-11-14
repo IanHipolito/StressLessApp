dba5d86977c07d5831e20c37175e455695e888e4
unknown <ian.hipolito04@gmail.com>
2023-11-14 23:55:53 +0000

    Starting Code

--- app/src/main/java/com/example/stressless/MainActivity.java
+++ app/src/main/java/com/example/stressless/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.stressless;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonMeditation = findViewById(R.id.buttonMeditation);
+        Button buttonBreathing = findViewById(R.id.buttonBreathing);
+        Button buttonMindfulness = findViewById(R.id.buttonMindfulness);
+
+        buttonMeditation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the MeditationActivity when the "Meditation" button is clicked
+                startActivity(new Intent(MainActivity.this, MeditationActivity.class));
+            }
+        });
+
+        buttonBreathing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the BreathingActivity when the "Breathing Exercises" button is clicked
+                startActivity(new Intent(MainActivity.this, BreathingActivity.class));
+            }
+        });
+
+        buttonMindfulness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the MindfulnessActivity when the "Mindfulness Challenges" button is clicked
+                startActivity(new Intent(MainActivity.this, MindfulnessActivity.class));
+            }
+        });
     }
+}
+
 }
---
+++ app/src/main/java/com/example/stressless/MindfulnessActivity.java
@@ -0,0 +1,4 @@
+package com.example.stressless;
+
+public class MindfulnessActivity {
+}
