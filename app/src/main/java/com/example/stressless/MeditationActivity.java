f702964a25b4a31c2aad83a64369cd11a13abaac
Ian Hipolito <ian.hipolito@sap.com>
2023-11-16 01:13:30 +0000

    Added Buttons For Play, Stop, Previous and Next For The Meditation Music

--- app/src/main/java/com/example/stressless/MeditationActivity.java
+++ app/src/main/java/com/example/stressless/MeditationActivity.java
@@ -12,6 +12,10 @@ public class MeditationActivity extends AppCompatActivity{
     private MediaPlayer mediaPlayer;
     private Button playButton;
     private Button stopButton;
+    private Button previousButton;
+    private Button nextButton;
+    private int[] songs = {R.raw.meditation_music, R.raw.meditation_music2, R.raw.meditation_music3, R.raw.meditation_music4};
+    private int currentSongIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -21,6 +25,8 @@ public class MeditationActivity extends AppCompatActivity{
         mediaPlayer = MediaPlayer.create(this, R.raw.meditation_music);
         playButton = findViewById(R.id.playButton);
         stopButton = findViewById(R.id.stopButton);
+        previousButton = findViewById(R.id.previousButton);
+        nextButton = findViewById(R.id.nextButton);
 
         playButton.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -46,6 +52,40 @@ public class MeditationActivity extends AppCompatActivity{
             }
         });
 
+        previousButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(currentSongIndex > 0){
+                    currentSongIndex--;
+                }
+                else{
+                    currentSongIndex = songs.length -1;
+                }
+                playSong(currentSongIndex);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(currentSongIndex < songs.length - 1){
+                    currentSongIndex++;
+                }
+                else{
+                    currentSongIndex = 0;
+                }
+                playSong(currentSongIndex);
+            }
+        });
+
+    }
+
+    private void playSong(int SongIndex){
+        if(mediaPlayer !=null){
+            mediaPlayer.release();
+        }
+        mediaPlayer = MediaPlayer.create(this, songs[SongIndex]);
+        mediaPlayer.start();
     }
 
     @Override
