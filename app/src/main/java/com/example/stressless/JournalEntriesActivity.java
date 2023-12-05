4b6bcca405a8d46ee603b22e25603bad02048e39
Ian Hipolito <ian.hipolito@sap.com>
2023-12-05 04:07:34 +0000

    More Progress To Room Database And Can Now Access Journal Entries

---
+++ app/src/main/java/com/example/stressless/JournalEntriesActivity.java
@@ -0,0 +1,49 @@
+package com.example.stressless;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
+
+import com.example.stressless.database.JournalEntryDatabase;
+import com.example.stressless.database.entities.JournalEntriesAdapter;
+import com.example.stressless.database.entities.JournalEntry;
+import java.util.List;
+
+public class JournalEntriesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private JournalEntriesAdapter adapter;
+    private JournalEntryDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal_entries);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        db = Room.databaseBuilder(getApplicationContext(),
+                JournalEntryDatabase.class, "journal-entry-database").build();
+
+        loadJournalEntries();
+    }
+
+    private void loadJournalEntries() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<JournalEntry> entries = db.journalEntryDAO().getAllEntries();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter = new JournalEntriesAdapter(entries);
+                        recyclerView.setAdapter(adapter);
+                    }
+                });
+            }
+        }).start();
+    }
+}
