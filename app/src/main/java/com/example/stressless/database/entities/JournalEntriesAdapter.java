eacb870e144d41a68b2cb8e0cde90dac65420296
Ian Hipolito <ian.hipolito@sap.com>
2023-12-08 15:56:37 +0000

    Progress on styling recycler view

--- app/src/main/java/com/example/stressless/database/entities/JournalEntriesAdapter.java
+++ app/src/main/java/com/example/stressless/database/entities/JournalEntriesAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -51,7 +52,7 @@ public class JournalEntriesAdapter extends RecyclerView.Adapter<JournalEntriesAd
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView entryTextView;
-        Button deleteButton;
+        ImageButton deleteButton;
 
         public ViewHolder(View view) {
             super(view);
